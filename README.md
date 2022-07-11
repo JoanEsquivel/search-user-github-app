f403868be1ff4d8c68173c769246367b43a9423b
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-07-10 18:18:46 -0700

    SPA finished

---
+++ README.md
@@ -0,0 +1,25 @@
+# curso-vue-3-desde-cero
+
+## Repositorio comprobante de haber finalizado el curso de Vue 3 desde Cero en Escuela Vue ⭐️
+
+Puedes acceder [desde aquí](https://www.escuelavue.es/cursos/curso-vue-3-desde-cero/).
+
+![Curso Vue 3](https://a.storyblok.com/f/87002/1280x720/18667defa9/curso-vue-3-desde-cero.jpg)
+
+> Aprender Vue desde cero es (además de una delicia) una de las mejores decisiones profesionales que has podido tomar. Enhorabuena 👏🏻
+
+Durante el Curso de Vue 3 aprendi qué es Vue, cómo funciona Vue y cuáles son sus novedades, por ejemplo:
+
+- Crear proyectos con Vue 3.
+- Usar JavaScript asíncrono con Vue.
+- Reactividad Vue.
+- Mejoras Vue 3.
+- Métodos y eventos Vue.
+- Renderizado condicional.
+- Atributos dinámicos.
+- Directivas.
+- Estilos y clases dinámicos.
+- Manejo de estado.
+- Persistencia.
+- Transiciones Vue.
+- Componentes Vue.
---
+++ js/main.js
@@ -0,0 +1,81 @@
+const API = "https://api.github.com/users/";
+const requestMaxTimeMs = 3000;
+
+const app = Vue.createApp({
+  data() {
+    return {
+      search: null,
+      result: null,
+      error: null,
+      favorites: new Map(),
+    };
+  },
+  methods: {
+    async doSearch() {
+      this.result = this.error = false;
+      const foundInFavorites = this.favorites.get(this.search);
+
+      const shouldRequestAgain = (() => {
+        if (!!foundInFavorites) {
+          const { lastRequestTime } = foundInFavorites;
+          const now = Date.now();
+          return now - lastRequestTime > requestMaxTimeMs;
+        }
+        return false;
+      })(); //IIFE
+
+      if (!!foundInFavorites && !shouldRequestAgain) {
+        console.log("Found and using cached data");
+        return (this.result = foundInFavorites);
+      }
+      try {
+        console.log("Not found or cached version is too old");
+        const response = await fetch(API + this.search);
+        if (!response.ok) throw new Error("User not found");
+        const data = await response.json();
+        console.log(data);
+        this.result = data;
+        foundInFavorites.lastRequestTime = Date.now();
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.search = null;
+      }
+    },
+    addFavorite() {
+      this.result.lastRequestTime = Date.now();
+      this.favorites.set(this.result.login, this.result);
+      this.updateStorage();
+    },
+    removeFavorite() {
+      this.favorites.delete(this.result.login);
+      this.updateStorage();
+    },
+    updateStorage() {
+      window.localStorage.setItem(
+        "favorites",
+        JSON.stringify(this.allFavorites)
+      );
+    },
+    showFavorite(favorite) {
+      this.result = favorite;
+    },
+  },
+  computed: {
+    isFavorite() {
+      return !this.favorites.has(this.result.login);
+    },
+    allFavorites() {
+      return Array.from(this.favorites.values());
+    },
+  },
+  created() {
+    const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"));
+    if (savedFavorites?.length) {
+      const favorites = new Map(
+        savedFavorites.map((favorite) => [favorite.login, favorite])
+      );
+      this.favorites = favorites;
+    }
+  },
+});
